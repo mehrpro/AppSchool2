6e17fbbe920d1b0a084ded3f9258ed8f199313c3
SEO <SEO@SEO>
2020-08-21 22:11:20 +0430

    990531-2

---
+++ SchoolApp.Repositoreis/SqlDbTools.cs
@@ -0,0 +1,40 @@
+﻿using SchoolApp.RepositoryAbstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace SchoolApp.Repositoreis
+{
+    public class SqlDbTools : IDbTools
+    {
+        private MySqlConnectionStringBuilder connectionStringBuilder;
+        private string tempDbName;
+        public async Task<bool> CheckDatabaseExists()
+        {
+            return true;
+        }
+
+        public async Task<bool> CheckDBConnection()
+        {
+            return true;
+
+        }
+
+        public async Task<bool> CreateDatabase(string dbScript)
+        {
+            return true;
+
+        }
+
+        public void RefreshConnectionString()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["mySQL"].ConnectionString;
+            connectionStringBuilder = new MySqlConnectionStringBuilder(connectionString);
+            connectionStringBuilder.init
+        }
+    }
+}
---
+++ SchoolApp.RepositoryAbstracts/IDbTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolApp.RepositoryAbstracts
+{
+    public interface IDbTools
+    {
+        Task<bool> CheckDBConnection();
+        Task<bool> CheckDatabaseExists();
+        Task<bool> CreateDatabase(string dbScript);
+        void RefreshConnectionString();
+    }
+}
